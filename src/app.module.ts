157fc0447a060069e6c0bda1db9ecc9f378a9342
MiniGT <bendanillo539@gmail.com>
2025-10-08 18:11:57 +0800

    feat: replace DiscordModule with CanvasModule in AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { DiscordModule } from './discord/discord.module';
 import { ConfigModule } from '@nestjs/config';
+import { CanvasModule } from './canvas/canvas.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: ".env.development"
   }), 
-  UsersModule, AuthModule, DiscordModule],
+  UsersModule, AuthModule, CanvasModule],
   controllers: [AppController],
   providers: [AppService],
 })
