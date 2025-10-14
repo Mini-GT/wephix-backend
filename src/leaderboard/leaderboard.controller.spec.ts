71fd96d0f6a18d42a86f42a56ca664975bf868b4
MiniGT <bendanillo539@gmail.com>
2025-10-14 16:58:35 +0800

    feat: implement leaderboard module with controller, service, and DTOs

---
+++ src/leaderboard/leaderboard.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LeaderboardController } from './leaderboard.controller';
+import { LeaderboardService } from './leaderboard.service';
+import { PrismaModule } from '../prisma/prisma.module';
+
+describe('LeaderboardController', () => {
+  let controller: LeaderboardController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
+      controllers: [LeaderboardController],
+      providers: [LeaderboardService],
+    }).compile();
+
+    controller = module.get<LeaderboardController>(LeaderboardController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
---
+++ src/leaderboard/leaderboard.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateLeaderboardDto } from './dto/create-leaderboard.dto';
+import { UpdateLeaderboardDto } from './dto/update-leaderboard.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class LeaderboardService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createLeaderboardDto: CreateLeaderboardDto) {
+    return 'This action adds a new leaderboard';
+  }
+
+  async findAll() {
+    const users = await this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        totalPixelsPlaced: true,
+        discord: {
+          select: {
+            global_name: true,
+            username: true,
+          },
+        },
+      },
+    });
+
+    if (!users) {
+      throw new NotFoundException('No users found');
+    }
+
+    return users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} leaderboard`;
+  }
+
+  update(id: number, updateLeaderboardDto: UpdateLeaderboardDto) {
+    return `This action updates a #${id} leaderboard`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} leaderboard`;
+  }
+}
